8cb88b16a381177722df61a4fdb20f74a18d44e3
appstechemail <appstechemail@gmail.com>
2024-06-23 17:51:41 +0530

    Commit in settings related to ALLOWED_HOSTS

--- ParadigmPortal/settings.py
+++ ParadigmPortal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv("SECRET_KEY", default="Change_me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG", True)
 
-ALLOWED_HOSTS = ["ALLOWED_HOSTS", 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ["ALLOWED_HOSTS", "https://xploreblog.onrender.com"]
 
 
 # Application definition
